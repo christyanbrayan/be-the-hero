32df92f5fef62cb16d962d06406278c88bf11fd7
christyanbrayan <christyankev@gmail.com>
2020-03-23 17:33:35 -0300

    day one - update response to json format

--- aulas/backend/index.js
+++ aulas/backend/index.js
@@ -3,7 +3,10 @@ const express = require('express')
 const app = express()
 
 app.get('/', (request, response) => {
-  return response.send('Hello World')
+  return response.json({
+    evento: 'Semana OmniStack 11.0',
+    aluno: 'Christyan Brayan'
+  })
 })
 
 app.listen(3333)
