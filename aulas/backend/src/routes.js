bd5a3b7f8c0b1ab60e6b002295578b560d64487c
christyanbrayan <christyankev@gmail.com>
2020-03-25 14:36:27 -0300

    day two - change to a controller

--- aulas/backend/src/routes.js
+++ aulas/backend/src/routes.js
@@ -1,31 +1,10 @@
 const express = require('express')
-const crypto = require('crypto')
 
-const connection  = require('./database/connection')
+const OngController = require('./controllers/OngController')
 
 const routes = express.Router()
 
-routes.get('/ongs', async (request, response) => {
-  const ongs = await connection('ongs').select('*')
-  
-  return response.json(ongs)
-})
-
-routes.post('/ongs', async (request, response) => {
-  const { name, email, whatsapp, city, uf } = request.body
-  
-  const id = crypto.randomBytes(4).toString('HEX')
-  
-  await connection('ongs').insert({
-    id,
-    name,
-    email,
-    whatsapp,
-    city,
-    uf,
-  })
-  
-  return response.json({ id })
-})
+routes.get('/ongs', OngController.index)
+routes.post('/ongs', OngController.create)
 
 module.exports = routes
