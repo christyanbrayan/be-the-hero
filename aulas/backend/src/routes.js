c87e5b4a1e10c622805a018cabd1ca4b83263110
christyanbrayan <christyankev@gmail.com>
2020-03-25 12:19:22 -0300

    day two - update routes.js

--- aulas/backend/src/routes.js
+++ aulas/backend/src/routes.js
@@ -2,15 +2,12 @@ const express = require('express')
 
 const routes = express.Router()
 
-routes.post('/users', (request, response) => {
-  const body = request.body
+routes.post('/ongs', (request, response) => {
+  const data = request.body
   
-  console.log(body)
+  console.log(data)
   
-  return response.json({
-    evento: 'Semana OmniStack 11.0',
-    aluno: 'Christyan Brayan'
-  })
+  return response.json()
 })
 
 module.exports = routes
