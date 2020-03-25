277fbcd318a28d755a7724055fae29a48c3f7826
christyanbrayan <christyankev@gmail.com>
2020-03-25 12:44:15 -0300

    day two - import crypto to generate random numbers

--- aulas/backend/src/routes.js
+++ aulas/backend/src/routes.js
@@ -1,11 +1,12 @@
 const express = require('express')
+const crypto = require('crypto')
 
 const routes = express.Router()
 
 routes.post('/ongs', (request, response) => {
   const { name, email, whatsapp, city, uf } = request.body
   
-  
+  const id = crypto.randomBytes(4).toString('HEX')
   
   return response.json()
 })
