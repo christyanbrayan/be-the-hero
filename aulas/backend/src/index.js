4aac37e8242539eb6c507f70dcad0c86282018a3
christyanbrayan <christyankev@gmail.com>
2020-03-25 11:08:02 -0300

    day two - update index.js to tests with the insomnia

--- aulas/backend/src/index.js
+++ aulas/backend/src/index.js
@@ -2,7 +2,13 @@ const express = require('express')
 
 const app = express()
 
-app.get('/', (request, response) => {
+app.use(express.json())
+
+app.posts('/users', (request, response) => {
+  const body = request.body
+  
+  console.log(body)
+  
   return response.json({
     evento: 'Semana OmniStack 11.0',
     aluno: 'Christyan Brayan'
