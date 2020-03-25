b30979bdcdb235bcaff356dfaf509a776fae9c37
christyanbrayan <christyankev@gmail.com>
2020-03-25 14:20:45 -0300

    day two - update routes to list on database

--- aulas/backend/src/routes.js
+++ aulas/backend/src/routes.js
@@ -5,6 +5,12 @@ const connection  = require('./database/connection')
 
 const routes = express.Router()
 
+routes.get('/ongs', async (request, response) => {
+  const ongs = await connection('ongs').select('*')
+  
+  return response.json(ongs)
+})
+
 routes.post('/ongs', async (request, response) => {
   const { name, email, whatsapp, city, uf } = request.body
   
