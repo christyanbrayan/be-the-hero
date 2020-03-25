47caa2e67b53b1ce4457e1972d3db3653d338775
christyanbrayan <christyankev@gmail.com>
2020-03-25 11:56:52 -0300

    day two - update file to create table of database

--- aulas/backend/src/database/migrations/20200325113138_create_ongs.js
+++ aulas/backend/src/database/migrations/20200325113138_create_ongs.js
@@ -1,8 +1,15 @@
 
 exports.up = function(knex) {
-  
+  knex.schema.createTable('ongs', function (table) {
+    table.string('id').primary()
+    table.string('name').notNullable()
+    table.string('email').notNullable()
+    table.string('whatsapp').notNullable()
+    table.string('city').notNullable()
+    table.string('uf', 2).notNullable()
+  })
 };
 
 exports.down = function(knex) {
-  
+  knex.schema.dropTable('ongs')
 };
