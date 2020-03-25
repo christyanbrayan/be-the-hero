00e57b07cabd10304f0fd6eb8d70eba5adfbf844
christyanbrayan <christyankev@gmail.com>
2020-03-25 14:14:37 -0300

    day two - connection with database

--- aulas/backend/src/routes.js
+++ aulas/backend/src/routes.js
@@ -1,14 +1,25 @@
 const express = require('express')
 const crypto = require('crypto')
 
+const connection  = require('./database/connection')
+
 const routes = express.Router()
 
-routes.post('/ongs', (request, response) => {
+routes.post('/ongs', async (request, response) => {
   const { name, email, whatsapp, city, uf } = request.body
   
   const id = crypto.randomBytes(4).toString('HEX')
   
-  return response.json()
+  await connection('ongs').insert({
+    id,
+    name,
+    email,
+    whatsapp,
+    city,
+    uf,
+  })
+  
+  return response.json({ id })
 })
 
 module.exports = routes
