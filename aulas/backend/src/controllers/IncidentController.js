169206a4e2f2e7c8c502b4df779fdef038d7f09e
christyanbrayan <christyankev@gmail.com>
2020-03-25 16:28:53 -0300

    day two - update to do pagination

--- aulas/backend/src/controllers/IncidentController.js
+++ aulas/backend/src/controllers/IncidentController.js
@@ -2,7 +2,16 @@ const connection = require('../database/connection')
 
 module.exports = {
   async index(request, response) {
-    const incidents = await connection('incidents').select('*')
+    const { page = 1 } = request.query
+
+    const [count] = await connection('incidents').count()
+
+    const incidents = await connection('incidents')
+      .limit(5)
+      .offset((page - 1) * 5 )
+      .select('*')
+    
+    response.header('X-Total-Count', count['count(*)'])
     
     return response.json(incidents)
   },
