f4b285781aad32bf7ca47643d54a695376ca9c21
christyanbrayan <christyankev@gmail.com>
2020-03-26 09:31:57 -0300

    day three - update with other way

--- frontend/src/Header.js
+++ frontend/src/Header.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-export default function Header(props) {
+export default function Header({ children }) {
   return (
     <header>
-      <h1> {props.children} </h1>
+      <h1> {children} </h1>
     </header>
   )
 }
