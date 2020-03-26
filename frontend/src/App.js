90d015d99b0f588fc373f86d98d0d812182a0706
christyanbrayan <christyankev@gmail.com>
2020-03-26 09:30:53 -0300

    day three - update with other way

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,9 @@ import Header from './Header'
 
 function App() {
   return (
-    <Header title="Semana OmniStack" />
+    <Header>
+      Semana OmniStack
+    </Header>
   );
 }
 
--- frontend/src/Header.js
+++ frontend/src/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Header(props) {
   return (
     <header>
-      <h1> {props.title} </h1>
+      <h1> {props.children} </h1>
     </header>
   )
 }
