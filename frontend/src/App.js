f00d7f8e4987328abd2279de9f41abe432debf63
christyanbrayan <christyankev@gmail.com>
2020-03-26 09:42:22 -0300

    day three - function increment

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Header from './Header'
 
 function App() {
+  const [counter, setCounter] = useState(0)
+  
+  function increment() {
+    setCounter(counter + 1)
+  }
+
   return (
-    <Header>
-      Semana OmniStack
-    </Header>
+    <div>
+      <Header> Contador: {counter} </Header>
+      <button onClick={increment}> Incrementar </button>
+    </div>
   );
 }
 
