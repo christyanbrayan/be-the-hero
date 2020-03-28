abf5b9261f418dd81236ecccba8a67a52f312889
christyanbrayan <christyankev@gmail.com>
2020-03-28 18:42:09 -0300

    day four - pages structure

--- mobile/App.js
+++ mobile/App.js
@@ -1,24 +1,10 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}> Hello OmniStack! </Text>
+    <View>
+      <Text> Hello OmniStack! </Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#7159c1',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    color: '#fff',
-    fontSize: 20,
-    fontWeight: 'bold'
-  }
-});
