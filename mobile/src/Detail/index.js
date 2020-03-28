c06be65822b8f98435751e476cde01ff1d5bd850
christyanbrayan <christyankev@gmail.com>
2020-03-28 18:47:03 -0300

    day four - initial index of pages

---
+++ mobile/src/Detail/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { View } from 'react-native'
+
+export default function Detail() {
+  return (
+    <View />
+  )
+}
---
+++ mobile/src/pages/Incidents/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { View } from 'react-native'
+
+export default function Incidents() {
+  return (
+    <View />
+  )
+}
