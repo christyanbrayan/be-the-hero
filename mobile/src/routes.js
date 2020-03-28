c1f9a35f6f0d4166ec39a69c84411d31c51815f8
christyanbrayan <christyankev@gmail.com>
2020-03-28 19:33:37 -0300

    day four - remove header

--- mobile/src/routes.js
+++ mobile/src/routes.js
@@ -10,7 +10,7 @@ import Detail from './pages/Detail'
 export default function Routes() {
   return (
     <NavigationContainer>
-      <AppStack.Navigator>
+      <AppStack.Navigator screenOptions={ {headerShown: false} }>
         <AppStack.Screen name="Incidents" component={Incidents} />
         <AppStack.Screen name="Detail" component={Detail} />
       </AppStack.Navigator>
