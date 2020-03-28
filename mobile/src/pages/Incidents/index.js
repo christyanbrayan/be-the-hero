44667a4f1e363350bf39ba44ed6c9d496e51bc41
christyanbrayan <christyankev@gmail.com>
2020-03-28 19:48:12 -0300

    day four - logos and initial formatting of incidents page

--- mobile/src/pages/Incidents/index.js
+++ mobile/src/pages/Incidents/index.js
@@ -1,8 +1,21 @@
 import React from 'react'
-import { View } from 'react-native'
+import { View, Image, Text } from 'react-native'
+
+import logoImg from '../../assets/logo.png'
+
+import styles from './styles'
 
 export default function Incidents() {
   return (
-    <View />
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Image source={logoImg} />
+        <Text style={styles.headerText}>
+          Total de <Text style={styles.headerTextBold}>0 casos</Text>.
+        </Text>
+      </View>
+      <Text style={styles.title}> Bem-vindo! </Text>
+      <Text style={styles.description}> Escolha um dos casos abaixo e salve o dia. </Text>
+    </View>
   )
 }
