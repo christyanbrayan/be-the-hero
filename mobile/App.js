fec8d76ea991c958155fa6a9c1d98e8b6e871ba7
christyanbrayan <christyankev@gmail.com>
2020-03-28 18:26:21 -0300

    day four - hello world formatting

--- mobile/App.js
+++ mobile/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text> Hello OmniStack! </Text>
+      <Text style={styles.title}> Hello OmniStack! </Text>
     </View>
   );
 }
@@ -12,8 +12,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#7159c1',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    color: '#fff',
+    fontSize: 20,
+    fontWeight: 'bold'
+  }
 });
