af1bac3fe806326cae7ab26030568a5e1f0e1ce9
christyanbrayan <christyankev@gmail.com>
2020-03-29 02:09:14 -0300

    day four - finish incidents page

--- mobile/src/pages/Incidents/index.js
+++ mobile/src/pages/Incidents/index.js
@@ -1,11 +1,17 @@
 import React from 'react'
-import { View, Image, Text } from 'react-native'
+import { Feather } from '@expo/vector-icons'
+import { useNavigation } from '@react-navigation/native'
+import { View, FlatList, Image, Text, TouchableOpacity } from 'react-native'
 
 import logoImg from '../../assets/logo.png'
 
 import styles from './styles'
 
 export default function Incidents() {
+  const navigation = useNavigation()
+  function navigateToDetail() {
+    navigation.navigate('Detail')
+  }
   return (
     <View style={styles.container}>
       <View style={styles.header}>
@@ -16,6 +22,24 @@ export default function Incidents() {
       </View>
       <Text style={styles.title}> Bem-vindo! </Text>
       <Text style={styles.description}> Escolha um dos casos abaixo e salve o dia. </Text>
+
+      <FlatList data={[1, 2, 3]} style={styles.incidentList} keyExtractor={incident => String(incident)} showsVerticalScrollIndicator={false} renderItem={() => (
+        <View style={styles.incident}>
+          <Text style={styles.incidentProperty}> ONG: </Text>
+          <Text style={styles.incidentValue}> APAD </Text>
+
+          <Text style={styles.incidentProperty}> CASO: </Text>
+          <Text style={styles.incidentValue}> Cadelinha atropelada </Text>
+
+          <Text style={styles.incidentProperty}> VALOR: </Text>
+          <Text style={styles.incidentValue}> 120,00 </Text>
+
+          <TouchableOpacity style={styles.detailsButton} onPress={navigateToDetail}>
+            <Text style={styles.detailsButtonText}> Ver mais detalhes </Text>
+            <Feather name="arrow-right" size={16} />
+          </TouchableOpacity>
+        </View>
+      )} />
     </View>
   )
 }
