ada6891ca6e2ffa818aa6d42ff0a15fd3e0b9849
christyanbrayan <christyankev@gmail.com>
2020-03-31 16:29:01 -0300

    day five - a unit test

--- backend/src/controllers/OngController.js
+++ backend/src/controllers/OngController.js
@@ -1,4 +1,4 @@
-const crypto = require('crypto')
+const generateUniqueId = require('../utils/generateUniqueId')
 const connection  = require('../database/connection')
 
 module.exports = {
@@ -11,7 +11,7 @@ module.exports = {
   async create(request, response) {
     const { name, email, whatsapp, city, uf } = request.body
 
-    const id = crypto.randomBytes(4).toString('HEX')
+    const id = generateUniqueId()
 
     await connection('ongs').insert({
       id,
